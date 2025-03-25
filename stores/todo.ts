39534de4a548d15d4bbc26e5e006a5473a4888c1
Rol4nd <mail@rolandfranke.nl>
2025-03-25 17:16:23 +0100

    Revert "Fixed bug in store"
    This reverts commit fb21971895165075f74c7762691a9c3ae0e78fad.

--- stores/todo.ts
+++ stores/todo.ts
@@ -27,37 +27,52 @@ export const useTodoStore = defineStore("todo", {
   actions: {
     loadFromLocalStorage(): boolean {
       const storedTodos = localStorage.getItem("todos");
-      const storedLocalTodos = localStorage.getItem("localTodos");
 
       if (storedTodos) this.todos = JSON.parse(storedTodos);
-      if (storedLocalTodos) this.localTodos = JSON.parse(storedLocalTodos);
 
-      return !!(storedTodos || storedLocalTodos);
+      return !!storedTodos;
     },
 
     saveToLocalStorage() {
-      localStorage.setItem("todos", JSON.stringify(this.todos));
-      localStorage.setItem("localTodos", JSON.stringify(this.localTodos));
+      localStorage.setItem("todos", JSON.stringify(this.allTodos));
     },
 
     async fetchTodos(limit: number = 5) {
-      try {
-        const response = await axios.get<TodoApiResponse>(
-          `${API_URL}?limit=${limit}`,
-        );
-        this.todos = response.data.todos;
-        this.saveToLocalStorage();
-      } catch (error) {
-        console.error("Error fetching todos:", error);
+      // Load todos from local storage
+      const storedTodos = localStorage.getItem("todos");
+
+      if (storedTodos) {
+        this.localTodos = JSON.parse(storedTodos);
+      }
+
+      // Fetch todos from API only if there are no stored todos
+      if (this.localTodos.length === 0) {
+        try {
+          const response = await axios.get<TodoApiResponse>(
+            `${API_URL}?limit=${limit}`,
+          );
+
+          this.todos = response.data.todos;
+          this.saveToLocalStorage();
+        } catch (error) {
+          console.error("Error fetching todos:", error);
+        }
       }
     },
 
     async addTodo(todo: Omit<Todo, "id">) {
       try {
+        // Simulate adding a new todo
         const response = await axios.post<Todo>(`${API_URL}/add`, todo, {
           headers: { "Content-Type": "application/json" },
         });
-        const newTodo = { ...response.data, isLocal: true, id: Date.now() };
+
+        // Create a new todo object with a unique ID
+        const newTodo = {
+          ...response.data,
+          isLocal: true,
+          id: Date.now(),
+        };
 
         this.localTodos.unshift(newTodo);
         this.saveToLocalStorage();
@@ -68,6 +83,7 @@ export const useTodoStore = defineStore("todo", {
 
     async removeTodo(id: number) {
       const localIndex = this.localTodos.findIndex((todo) => todo.id === id);
+
       if (localIndex !== -1) {
         this.localTodos.splice(localIndex, 1);
         this.saveToLocalStorage();
@@ -75,12 +91,14 @@ export const useTodoStore = defineStore("todo", {
       }
 
       const apiIndex = this.todos.findIndex((todo) => todo.id === id);
+
       if (apiIndex !== -1) {
         try {
           const response = await axios.delete<{
             isDeleted: boolean;
             deletedOn: string;
           }>(`${API_URL}/${id}`);
+
           if (response.data.isDeleted) {
             this.todos.splice(apiIndex, 1);
             this.saveToLocalStorage();
@@ -93,6 +111,7 @@ export const useTodoStore = defineStore("todo", {
 
     async updateTodoStatus(id: number, completed: boolean) {
       const localIndex = this.localTodos.findIndex((todo) => todo.id === id);
+
       if (localIndex !== -1) {
         this.localTodos[localIndex].completed = completed;
         this.saveToLocalStorage();
@@ -100,6 +119,7 @@ export const useTodoStore = defineStore("todo", {
       }
 
       const apiIndex = this.todos.findIndex((todo) => todo.id === id);
+
       if (apiIndex !== -1) {
         try {
           const response = await axios.put<Todo>(
@@ -107,6 +127,7 @@ export const useTodoStore = defineStore("todo", {
             { completed },
             { headers: { "Content-Type": "application/json" } },
           );
+
           this.todos[apiIndex].completed = response.data.completed;
           this.saveToLocalStorage();
         } catch (error) {
@@ -117,13 +138,12 @@ export const useTodoStore = defineStore("todo", {
   },
 });
 
-// Interfaces
 export interface Todo {
   id: number;
   todo: string;
   completed: boolean;
   userId: number;
-  isLocal?: boolean;
+  isLocal?: boolean; // Flag to indicate if the todo was added locally
   isDeleted?: boolean;
   deletedOn?: string;
 }
